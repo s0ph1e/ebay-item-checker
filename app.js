feb8c522df62bd42d7525b8e4423638a59969037
s0ph1e <sonya1912@gmail.com>
2014-11-16 22:19:34 +0200

    Update parsePrice according to eBay new markup, minor refactoring

--- app.js
+++ app.js
@@ -1,164 +1,139 @@
-var url = require('url'),
-  cheerio = require('cheerio'),
-  request = require('request'),
-  config = require('./config.js'),
-  nodemailer = require('nodemailer'),
-  smtpTransport = nodemailer.createTransport('SMTP', config.mail),
-  NodeCache = require('node-cache'),
-  cache = new NodeCache({stdTTL: config.sameItemDelay});
-  
-function isEmptyObject(obj) {
-  return !Object.keys(obj).length;
-}
-  
-function compare(a,b){
-  if(a.total < b.total){
-     return -1;
-  } else if(a.total > b.total){
-    return 1;
-  } else {
-    return 0;
-  }
+var url = require('url');
+var cheerio = require('cheerio');
+var request = require('request');
+var nodemailer = require('nodemailer');
+var NodeCache = require('node-cache');
+var _ = require('lodash');
+
+var config = require('./config2.js');
+var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+var cache = new NodeCache({stdTTL: config.sameItemDelay});
+
+
+function createEmailText(items) {
+  return _.reduce(items, function (text, item) {
+    var itemLine = '<a href="' + item.url + '" + target="_blank">' + item.name +
+      ' $' + item.total + ' (price $' + item.price + ' + shipping $' +
+      item.ship + ')</a><br>';
+    return text += itemLine;
+  }, '');
 }
 
-function filter(price){
-  return function(obj) {
-    if(obj.total > price){
-      return false;
+function sendEmail(item, results) {
+  var emailTo = _.compact([config.email, item.email]).join(',');
+  var emailText = createEmailText(results);
+  var emailSubject = item.name + ' $' + results[0].total;
+
+  smtpTransport.sendMail({
+    to: emailTo,
+    subject: emailSubject,
+    html: emailText
+  }, function (error, response) {
+    if (error) {
+      console.log(error);
     } else {
-      var cached = cache.get(obj.url);
-      
-      if(isEmptyObject(cached)){
-        cache.set(obj.url);
-        return true;
-      } else {
-        console.log(obj.url + ' cached');
-        return false;
-      }
+      console.log('Message sent: ' + response.message);
     }
-  }
+  });
 }
 
-function createEmail(items){
-  var text = '';
-  for(var i = 0, len = items.length; i < len; i++){
-    text += '<a href="' + items[i].url + '" + target="_blank">' + items[i].name + 
-      ' $' + items[i].total + ' (price $' +items[i].price + ' + shipping $' + 
-      items[i].ship + ')</a><br>';
-  }
-  return text;
-}
-  
-function parsePrices(html){
-  var $ = cheerio.load(html)
-    resultContainer = '#ResultSetItems',
-    priceRegexp = /(\d{1,3},)*\d+\.\d{2}/,
-    results = [];
-  
+function parsePrices(html) {
+  var $ = cheerio.load(html);
+  var resultContainer = '#ListViewInner';
+  var priceRegexp = /(\d{1,3},)*\d+\.\d{2}/;
+  var results = [];
+
   // Loop over all items on page
-  $('table', resultContainer).each(function(){
-  
-    var nameBlock = $(this).find('td.dtl').find('.ittl h3 a'),
-      totalPriceBlock = $(this).find('td.prc'),
-      itemPriceBlock = totalPriceBlock.find('.g-b[itemprop="price"]'),
-      shippingBlock = totalPriceBlock.find('.ship'),
-      url = nameBlock.attr('href'),
-      name = nameBlock.text(),
-      price,
-      shipping;
-    
+  $('li.lvresult', resultContainer).each(function () {
+    var self = $(this);
+
+    var nameBlock = self.find('h3.lvtitle').find('a');
+    var totalPriceBlock = self.find('ul.lvprices');
+    var itemPriceBlock = totalPriceBlock.find('li.lvprice').find('.g-b');
+    var shippingBlock = totalPriceBlock.find('li.lvshipping');
+
+    var url = nameBlock.attr('href');
+    var name = nameBlock.text();
+    var price, shipping, total;
+
     // Extract price
-    if(itemPriceBlock.length){
-    
+    if (!_.isEmpty(itemPriceBlock)) {
+
       price = itemPriceBlock.text();
       price = price.match(priceRegexp)[0].replace(',', '');
-      price = Number(Number(price).toFixed(2));
+      price = +((+price).toFixed(2));
 
     }
-    
+
     // Detect if shipping is set
-    if(shippingBlock.find('.fee').length){      // Shipping price is set
-    
-      shipping = shippingBlock.find('.fee').text();  
+    if (!_.isEmpty(shippingBlock.find('.fee'))) {      // Shipping price is set
+
+      shipping = shippingBlock.find('.fee').text();
       shipping = shipping.match(priceRegexp)[0].replace(',', '');
-      shipping = Number(Number(shipping).toFixed(2));
-      
-    } else if(shippingBlock.find('span .gfsp').length){  // Free shipping
-    
+      shipping = +((+shipping).toFixed(2));
+
+    } else if (!_.isEmpty(shippingBlock.find('.gvbfree'))) {  // Free shipping
+
       shipping = 0;
-      
-    } else if(shippingBlock.html().indexOf('not specified')){ // Not specified
-    
+
+    } else if (shippingBlock.text().indexOf('not specified')) { // Not specified
+
       console.log(url + ' shipping not specified');
-      return; 
-   
-   }
-    
-    if(!(typeof price == 'number' && typeof shipping == 'number')){
+      return;
+
+    }
+
+    if (!(typeof price == 'number' && typeof shipping == 'number')) {
       console.log(url + 'something wrong with price or shipping');
       return;
     }
-    
+
     total = price + shipping;
-    total = Number(total.toFixed(2));
+    total = +total.toFixed(2);
 
     results.push({
-      name: name, 
-      url: url, 
-      price: price, 
+      name: name,
+      url: url,
+      price: price,
       ship: shipping,
       total: total
     });
   });
-  
+
   return results;
 }
 
-function checkPrices(item, results){
-
+function filterByPrice(item, results) {
   // get item with wanted price which are not cached
-  var itemsFound = results.filter(filter(item.price));
-  
-  if(itemsFound.length > 0){
-
-    // Email addresses for send notifications
-    emailTo = config.email;
-
-    // Add item emails if specified
-    if (typeof(item.email) != 'undefined'){
-      emailTo += ',' + item.email;
+  return _.filter(results, function (result) {
+    if (result.total <= item.price && _.isEmpty(cache.get(result.url))) {
+      cache.set(result.url);
+      return true;
     }
-
-    smtpTransport.sendMail({
-       to: emailTo, 
-       subject: item.name + ' $' + itemsFound[0].total,
-       html: createEmail(itemsFound),
-    }, function(error, response){
-      if(error){
-        console.log(error);
-      }else{
-        console.log('Message sent: ' + response.message);
-      }
-    });
-  }
-}
-
-function checkItem(item){
-  console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''));
-  console.log('Checking ' + item.name);
-  request(item.url, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var results = parsePrices(body);
-      checkPrices(item, results);
-    }
+    return false;
   });
 }
-  
-function checkAllItems(){
-  for(var i = 0, len = config.items.length; i < len; i++){
-    checkItem(config.items[i]);
-  }
-}  
 
-checkAllItems();
-setInterval(checkAllItems, config.delay * 1000);
+function checkAll() {
+  _.each(config.items, function (item) {
+
+    console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''));
+    console.log('Checking ' + item.name);
+
+    request(item.url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+
+        var results = parsePrices(body);
+        var filtered = filterByPrice(item, results);
+
+        if (!_.isEmpty(filtered)) {
+          console.log(filtered);
+          sendEmail(item, filtered);
+        }
+      }
+    });
+  });
+}
+
+checkAll();
+setInterval(checkAll, config.delay * 1000);
